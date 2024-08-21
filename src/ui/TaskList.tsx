636d4771db02d670e5b2945c91cb0ac96baf9010
HuyNhay <pbhuy16072005@gmail.com>
2024-08-21 14:41:22 +0700

    refactor: combine useContext and useReducer to manage tasks state

--- src/ui/TaskList.tsx
+++ src/ui/TaskList.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import { useTasks } from './TasksProvider';
+import { TaskType } from '../utils/tasksReducer';
+import Task from './Task';
 
-interface TaskListProps {
-  children: React.ReactNode;
-}
+export default function TaskList() {
+  const tasks = useTasks() as TaskType[];
 
-export default function TaskList({ children }: TaskListProps) {
   return (
     <ul className="col-span-full grid grid-cols-8 gap-x-2 gap-y-4 lg:col-span-8 lg:col-start-3 lg:grid-cols-subgrid lg:gap-x-4">
-      {children}
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+        />
+      ))}
     </ul>
   );
 }
